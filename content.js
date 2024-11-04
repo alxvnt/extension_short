9dc3776ff07c4496c169c91868006eda1e130e68
alxvnt <alixvoinot1@gmail.com>
2024-11-04 17:11:31 +0100

    update hiding of all the short and the title

--- content.js
+++ content.js
@@ -1,10 +1,17 @@
 
 
 function masquerShort () {
-    const link = document.querySelectorAll('a[href*="/shorts/"]');
-    link.forEach(section => {
+    //const link = document.querySelectorAll('a[href*="/shorts/"]');
+    const shortHead = document.querySelectorAll("#rich-shelf-header-container");
+    //const viewNumber = document.querySelectorAll(".ShortsLockupViewModelHostMetadataSubhead");
+    const butt = document.querySelectorAll(".ShortsLockupViewModelHost");
+   shortHead.forEach(section => {
+        section.style.display = 'none';
+   })
+    butt.forEach(section => {
         section.style.display = 'none';
     });
+    
     console.log("test fonction");
 }
 
@@ -12,3 +19,5 @@ window.addEventListener('load', masquerShort);
 
 const observer = new MutationObserver(masquerShort);
 observer.observe(document.body, { childList: true, subtree: true });
+
+//yt-spec-touch-feedback-shape yt-spec-touch-feedback-shape--touch-response
